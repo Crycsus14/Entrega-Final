45f676b8e843af8d66739ef331e688e30fe9e78d
Sebastian Rubio <seba_rubio8@hotmail.com>
2024-04-20 00:09:12 -0300

    JS2

---
+++ Preentrega1/Js/main.js
@@ -0,0 +1,36 @@
+let bienvenida = prompt("Escribi tu nombre: ");
+alert("Hola DT "+ bienvenida );
+
+const jugadores = [ 
+    "Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4", "Jugador 5",
+    "Jugador 6", "Jugador 7", "Jugador 8", "Jugador 9", "Jugador 10",
+    "Jugador 11", "Jugador 12", "Jugador 13", "Jugador 14", "Jugador 15"
+];
+console.log("Estos son los posibles " , jugadores);
+
+let jugadoresSeleccionados = [];
+
+function equipo() {
+
+while (jugadoresSeleccionados.length < 5) {
+    let jugadorElegido = prompt(`Elige un jugador (${5 - jugadoresSeleccionados.length} restantes:}`);
+
+    if (!jugadores.includes(jugadorElegido)) {
+        alert("Por favor, convoca un jugador de la lista.");
+    } else if (jugadoresSeleccionados.includes(jugadorElegido)) {
+        alert("Ya elegiste a ese jugador. Por favor, eligí otro.");
+    } else {
+        jugadoresSeleccionados.push(jugadorElegido);
+    }
+}
+
+}
+equipo();
+
+console.log("Jugadores seleccionados para el 5 titular:", jugadoresSeleccionados);
+
+document.write("CONVOCATORIA DEL DT " , bienvenida , " ,tus elegidos son: ", jugadoresSeleccionados );
+
+alert("Tus jugadores para el 5 titular son: " + jugadoresSeleccionados);
+
+alert("MUCHA SUERTE EN EL TORNEO DT " + bienvenida + "!!");
